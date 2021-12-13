95e199d963813c94cd6bfc210c32ae682b5594d6
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-13 11:23:46 +0800

    Add files via upload

---
+++ class06.java
@@ -0,0 +1,20 @@
+
+public class class06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			int num=12;
+			int den[]= {12,0,3,0,0,4};
+			for(int i=0;i<12;i++)
+				System.out.println("ans="+num/den[i]);
+		}
+		catch(Exception e)
+		{
+			System.out.println("®·®»¨ì¨Ò¥~");
+		}
+
+	}
+
+}
