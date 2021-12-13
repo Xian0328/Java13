ae301990b2dc239118fba4bff76d9a716ed08f09
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-13 10:30:27 +0800

    Add files via upload

---
+++ class02.java
@@ -0,0 +1,18 @@
+
+public class class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=12,den=0;
+		if(den==0)
+		{
+			System.out.println("°£¼Æ¬°0");
+		}
+		else
+		{
+			int ans=num/den;
+		}
+		System.out.println("end of main()!!");
+	}
+
+}
