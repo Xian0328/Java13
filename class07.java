7f7f0f50a664c561e5b713473443a950d0c95006
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-13 11:41:38 +0800

    Add files via upload

---
+++ class07.java
@@ -0,0 +1,25 @@
+
+public class class07 {
+	public static void test(int num,int den)throws ArithmeticException
+	{
+		int ans=num/den;
+		System.out.println("ans="+ans);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			test(12,0);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("°£¼Æ¬°0");
+		}
+		finally
+		{
+			System.out.println("end of main()!!");
+		}
+	}
+
+}
---
+++ class08.java
@@ -0,0 +1,27 @@
+class Ctest
+{
+	public static void test(int num,int den)throws ArithmeticException
+	{
+		int ans=num/den;
+		System.out.println("ans="+ans);
+	}
+}
+public class class08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			Ctest.test(12,0);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("°£¼Æ¬°0");
+		}
+		finally
+		{
+			System.out.println("end of main()!!");
+		}
+	}
+
+}
