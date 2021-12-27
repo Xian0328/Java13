afdd27fd498e797e9921d7dedb30890d02a86015
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-27 20:30:42 +0800

    Add files via upload

---
+++ class15.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+class ArgumentOutOfBound extends Exception
+{}
+public class class15 {
+
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader buf;
+		String str;
+		int num;
+		
+		buf=new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("請輸入大於0的整數:");
+		str=buf.readLine();
+		num=Integer.parseInt(str);
+		mySqrt(num);
+	}
+	public static void mySqrt(int n)
+	{
+		try
+		{
+			if(n<0)
+				throw new ArgumentOutOfBound();
+			else
+			{
+				System.out.println("sqrt("+n+")="+Math.sqrt(n));
+			}
+		}
+		catch(ArgumentOutOfBound e)
+		{
+			System.out.println(n+"小於0");
+		}
+	}
+
+}
